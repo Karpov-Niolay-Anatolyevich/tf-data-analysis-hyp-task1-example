7ba2412337c1839775e32053ae1e06c9d1769998
Karpov-Niolay-Anatolyevich <130171336+Karpov-Niolay-Anatolyevich@users.noreply.github.com>
2023-04-13 21:07:35 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -17,17 +17,3 @@ def solution(x_success: int,
     # Не меняйте название функции и её аргументы
     return p_value < alpha # Ваш ответ, True или False
 
-  
-import pandas as pd
-import numpy as np
-from scipy.stats import kruskal
-
-chat_id = 1135291357 # Ваш chat ID, не меняйте название переменной
-
-def solution(x: np.array , y:np.array) -> bool:
-    alpha = 0.09
-    test_stat, p_value = kruskal(x, y)
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return p_value < alpha # Ваш ответ, True или False
