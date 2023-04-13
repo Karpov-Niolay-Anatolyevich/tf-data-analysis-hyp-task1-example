7331c692fb9ca4d0c0b2b0f5deb40cb2c6d6bb20
Karpov-Niolay-Anatolyevich <130171336+Karpov-Niolay-Anatolyevich@users.noreply.github.com>
2023-04-13 21:51:52 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import kruskal
+from scipy.stats import t
 
 chat_id = 1135291357 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array , y:np.array) -> bool:
+def solution(x: np.array) -> bool:
     alpha = 0.09 
-    test_stat, p_value = kruskal(x, y)
+    n=len(x)
+    mu = 500
+    t_stat=(x.mean()-mu) / (x.std() /(n**0.5))
+    crit_val=t.ppf(1-alpha, n-1)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return p_value < alpha # Ваш ответ, True или False
+    return t_stat > crit_val # Ваш ответ, True или False
