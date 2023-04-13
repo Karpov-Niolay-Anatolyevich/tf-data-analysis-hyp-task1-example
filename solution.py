0f050682a6c316ea7f1db2e89e2e0e15f733db0d
Karpov-Niolay-Anatolyevich <130171336+Karpov-Niolay-Anatolyevich@users.noreply.github.com>
2023-04-13 21:51:19 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import kruskal
 
 chat_id = 1135291357 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
+def solution(x: np.array , y:np.array) -> bool:
+    alpha = 0.09 
+    test_stat, p_value = kruskal(x, y)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 0.07
-    p_value = ttest_ind(x, y, alternative="greater").pvalue
     return p_value < alpha # Ваш ответ, True или False
